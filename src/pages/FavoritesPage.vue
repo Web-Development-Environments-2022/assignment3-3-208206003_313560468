5c14a14690b66a72392699fce452f778dd7914f8
roeybokoza <81579006+roeybokoza@users.noreply.github.com>
2022-06-24 17:27:49 +0300

    Second

---
+++ src/pages/FavoritesPage.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <div v-if="!$root.store.username">
+            <NotFound ></NotFound>
+        </div>
+    
+        <div v-else>
+            <h1 class="title"> Favorites Page</h1>
+            <RecipePreviewList title="Favourites Recipes" message="favorites" class="RandomRecipes center" />
+        </div>
+    </div>
+</template>
+
+<script>
+import RecipePreviewList from "../components/RecipePreviewList";
+import NotFound from "../pages/NotFoundPage";
+export default {
+    components: {
+        RecipePreviewList,
+        NotFound
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.RandomRecipes {
+    margin: 10px 0 10px;
+}
+
+.blur {
+    -webkit-filter: blur(5px);
+    /* Safari 6.0 - 9.0 */
+    filter: blur(2px);
+}
+
+::v-deep .blur .recipe-preview {
+    pointer-events: none;
+    cursor: default;
+}
+</style>
---
+++ src/store.js
@@ -0,0 +1,4 @@
+const state = {
+    server_domain: "http://localhost:3000"
+};
+export default state;
